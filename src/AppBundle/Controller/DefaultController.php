7cc6be154c0848bb5549726378d744f9a658625a
Linux <fc_murg@yahoo.it>
2017-04-26 08:06:04 +0200

    Avvio progetto

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,8 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        return $this->render('AppBundle:Default:index.html.twig');
+        return new Response("ok");
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
